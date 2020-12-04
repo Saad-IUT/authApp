19dabbbda6412304aa7d1f108e75c27fa8058150
Saad <saadbinjohir@gmail.com>
2020-12-04 21:30:16 +0600

    signup init

--- src/functions/AsyncStorage.js
+++ src/functions/AsyncStorage.js
@@ -1,8 +1,8 @@
-import AsyncStorage from '@react-native-community/async-storage'
+import LocalStorage from '@react-native-community/async-storage'
 
 const storeData = async (key, value) => {
   try {
-    await AsyncStorage.setItem(key, value)
+    await LocalStorage.setItem(key, value)
     // alert('Data Stored Successfully!')
   } catch (error) {
     alert(error)
@@ -12,7 +12,7 @@ const storeData = async (key, value) => {
 const storeDataJSON = async (key, value) => {
   try {
     const jsonValue = JSON.stringify(value)
-    await AsyncStorage.setItem(key, jsonValue)
+    await LocalStorage.setItem(key, jsonValue)
     alert('Data Stored Successfully!')
   } catch (error) {
     alert(error)
@@ -21,7 +21,7 @@ const storeDataJSON = async (key, value) => {
 
 const getData = async key => {
   try {
-    let data = await AsyncStorage.getItem(key)
+    let data = await LocalStorage.getItem(key)
     if (data != null) {
       return data
     } else {
@@ -34,7 +34,7 @@ const getData = async key => {
 
 const getDataJSON = async key => {
   try {
-    let data = await AsyncStorage.getItem(key)
+    let data = await LocalStorage.getItem(key)
     if (data != null) {
       const jsonData = JSON.parse(data)
       return jsonData
@@ -48,7 +48,7 @@ const getDataJSON = async key => {
 
 const removeData = async key => {
   try {
-    await AsyncStorage.removeItem(key)
+    await LocalStorage.removeItem(key)
     // alert('Data Removed Successfully')
   } catch (error) {
     alert(error)
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -5,6 +5,7 @@ import { FontAwesome, Feather, AntDesign, Ionicons } from '@expo/vector-icons'
 import globalStyles from '../styles/global'
 import { signUp } from '../context/actions/userActions'
 import { AppContext } from '../context/store'
+import { getDataJSON, storeDataJSON } from '../functions/AsyncStorage'
 
 const SignUpScreen = ({ navigation }) => {
   const [handle, setHandle] = useState('')
@@ -12,18 +13,47 @@ const SignUpScreen = ({ navigation }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
+  const [errors, setErrors] = useState({})
   const { ui, uiDispatch } = useContext(AppContext)
-  const { loading, disable, errors } = ui
-  const handleSubmit = () => {
-    signUp(
-      handle,
-      studentId,
-      email,
-      password,
-      confirmPassword,
-      navigation,
-      uiDispatch
-    )
+  const { loading, disable } = ui
+  const handleSubmit = async () => {
+    let users = await getDataJSON('user')
+    if (users) {
+      storeDataJSON('user', [
+        ...users,
+        {
+          handle,
+          studentId,
+          email,
+          password,
+          confirmPassword,
+          // navigation,
+          // uiDispatch,
+        },
+      ])
+    } else {
+      storeDataJSON('user', [
+        {
+          handle,
+          studentId,
+          email,
+          password,
+          confirmPassword,
+          // navigation,
+          // uiDispatch,
+        },
+      ])
+    }
+
+    // signUp(
+    //   handle,
+    //   studentId,
+    //   email,
+    //   password,
+    //   confirmPassword,
+    //   navigation,
+    //   uiDispatch
+    // )
   }
   return (
     <View style={globalStyles.authViewStyle}>
@@ -35,60 +65,68 @@ const SignUpScreen = ({ navigation }) => {
           placeholder='Name'
           onChangeText={currentInput => {
             setHandle(currentInput)
+            setErrors({ ...errors, handle: '' })
+            if (currentInput === '')
+              setErrors({ ...errors, handle: 'Must not be empty' })
           }}
           disabled={disable}
         />
-        {errors ? (
-          <Text style={globalStyles.errorTextStyle}>{errors.handle}</Text>
-        ) : null}
+        <Text style={globalStyles.errorTextStyle}>{errors.handle}</Text>
         <Input
           leftIcon={<Ionicons name='ios-school' size={24} color='black' />}
           placeholder='Student ID'
           onChangeText={currentInput => {
             setStudentId(currentInput)
+            setErrors({ ...errors, studentId: '' })
+            if (currentInput === '')
+              setErrors({ ...errors, studentId: 'Must not be empty' })
           }}
           disabled={disable}
         />
-        {errors ? (
-          <Text style={globalStyles.errorTextStyle}>{errors.studentId}</Text>
-        ) : null}
+        <Text style={globalStyles.errorTextStyle}>{errors.studentId}</Text>
         <Input
           leftIcon={<FontAwesome name='envelope' size={24} color='black' />}
           placeholder='E-mail Address'
           onChangeText={currentInput => {
             setEmail(currentInput)
+            setErrors({ ...errors, email: '' })
+            if (currentInput === '')
+              setErrors({ ...errors, email: 'Must not be empty' })
           }}
           disabled={disable}
         />
-        {errors ? (
-          <Text style={globalStyles.errorTextStyle}>{errors.email}</Text>
-        ) : null}
+        <Text style={globalStyles.errorTextStyle}>{errors.email}</Text>
+
         <Input
           placeholder='Password'
           leftIcon={<Feather name='key' size={24} color='black' />}
           secureTextEntry={true}
           onChangeText={currentInput => {
             setPassword(currentInput)
+            setErrors({ ...errors, password: '' })
+            if (currentInput === '')
+              setErrors({ ...errors, password: 'Must not be empty' })
           }}
           disabled={disable}
         />
-        {errors ? (
-          <Text style={globalStyles.errorTextStyle}>{errors.password}</Text>
-        ) : null}
+        <Text style={globalStyles.errorTextStyle}>{errors.password}</Text>
+
         <Input
           placeholder='Confirm Password'
           leftIcon={<Feather name='key' size={24} color='black' />}
           secureTextEntry={true}
           onChangeText={currentInput => {
             setConfirmPassword(currentInput)
+            setErrors({ ...errors, confirmPassword: '' })
+            if (currentInput === '')
+              setErrors({ ...errors, confirmPassword: 'Must not be empty' })
           }}
           disabled={disable}
         />
-        {errors ? (
-          <Text style={globalStyles.errorTextStyle}>
-            {errors.confirmPassword}
-          </Text>
-        ) : null}
+        <Text style={globalStyles.errorTextStyle}>
+          {errors.confirmPassword}
+        </Text>
+
         {loading ? (
           <Button
             icon={
